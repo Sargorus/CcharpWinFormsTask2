b32327315552038228d113b29820d543c563714a
Sargorus <Dima35206@yandex.ru>
2026-03-11 10:18:25 +0800

    Добавлено описание задания, названия кнопок и текстовых полей приведено в осмысленный вид

--- CcharpWinFormsTask2/Form1.Designer.cs
+++ CcharpWinFormsTask2/Form1.Designer.cs
@@ -28,19 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBoxInitialDeposit = new System.Windows.Forms.TextBox();
             this.TextLableInitialDeposit = new System.Windows.Forms.Label();
             this.TextLableLimitIncomeDeposit = new System.Windows.Forms.Label();
             this.textBoxLimitIncomeDeposit = new System.Windows.Forms.TextBox();
             this.TextLableLimitDeposit = new System.Windows.Forms.Label();
             this.textBoxLimitDeposit = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonMonthIncrease = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.labelTaskDescription = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxInitialDeposit
             // 
-            this.textBoxInitialDeposit.Location = new System.Drawing.Point(24, 50);
+            this.textBoxInitialDeposit.Location = new System.Drawing.Point(13, 126);
             this.textBoxInitialDeposit.Name = "textBoxInitialDeposit";
             this.textBoxInitialDeposit.Size = new System.Drawing.Size(100, 22);
             this.textBoxInitialDeposit.TabIndex = 0;
@@ -48,7 +50,7 @@
             // TextLableInitialDeposit
             // 
             this.TextLableInitialDeposit.AutoSize = true;
-            this.TextLableInitialDeposit.Location = new System.Drawing.Point(21, 31);
+            this.TextLableInitialDeposit.Location = new System.Drawing.Point(10, 107);
             this.TextLableInitialDeposit.Name = "TextLableInitialDeposit";
             this.TextLableInitialDeposit.Size = new System.Drawing.Size(233, 16);
             this.TextLableInitialDeposit.TabIndex = 1;
@@ -57,7 +59,7 @@
             // TextLableLimitIncomeDeposit
             // 
             this.TextLableLimitIncomeDeposit.AutoSize = true;
-            this.TextLableLimitIncomeDeposit.Location = new System.Drawing.Point(21, 89);
+            this.TextLableLimitIncomeDeposit.Location = new System.Drawing.Point(10, 165);
             this.TextLableLimitIncomeDeposit.Name = "TextLableLimitIncomeDeposit";
             this.TextLableLimitIncomeDeposit.Size = new System.Drawing.Size(524, 16);
             this.TextLableLimitIncomeDeposit.TabIndex = 2;
@@ -65,7 +67,7 @@
             // 
             // textBoxLimitIncomeDeposit
             // 
-            this.textBoxLimitIncomeDeposit.Location = new System.Drawing.Point(24, 108);
+            this.textBoxLimitIncomeDeposit.Location = new System.Drawing.Point(13, 184);
             this.textBoxLimitIncomeDeposit.Name = "textBoxLimitIncomeDeposit";
             this.textBoxLimitIncomeDeposit.Size = new System.Drawing.Size(98, 22);
             this.textBoxLimitIncomeDeposit.TabIndex = 3;
@@ -73,7 +75,7 @@
             // TextLableLimitDeposit
             // 
             this.TextLableLimitDeposit.AutoSize = true;
-            this.TextLableLimitDeposit.Location = new System.Drawing.Point(23, 151);
+            this.TextLableLimitDeposit.Location = new System.Drawing.Point(12, 227);
             this.TextLableLimitDeposit.Name = "TextLableLimitDeposit";
             this.TextLableLimitDeposit.Size = new System.Drawing.Size(552, 16);
             this.TextLableLimitDeposit.TabIndex = 4;
@@ -81,36 +83,46 @@
             // 
             // textBoxLimitDeposit
             // 
-            this.textBoxLimitDeposit.Location = new System.Drawing.Point(25, 170);
+            this.textBoxLimitDeposit.Location = new System.Drawing.Point(14, 246);
             this.textBoxLimitDeposit.Name = "textBoxLimitDeposit";
             this.textBoxLimitDeposit.Size = new System.Drawing.Size(97, 22);
             this.textBoxLimitDeposit.TabIndex = 5;
             // 
-            // button1
+            // buttonMonthIncrease
             // 
-            this.button1.Location = new System.Drawing.Point(30, 208);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 37);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonMonthIncrease.Location = new System.Drawing.Point(13, 274);
+            this.buttonMonthIncrease.Name = "buttonMonthIncrease";
+            this.buttonMonthIncrease.Size = new System.Drawing.Size(602, 33);
+            this.buttonMonthIncrease.TabIndex = 6;
+            this.buttonMonthIncrease.Text = "Отобразить через сколько ежемесячный процент дохода превысит заданный процент";
+            this.buttonMonthIncrease.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(31, 261);
+            this.button2.Location = new System.Drawing.Point(15, 313);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(123, 28);
+            this.button2.Size = new System.Drawing.Size(600, 28);
             this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
+            this.button2.Text = "Отобразить месяц когда размер влкад превысит лимит \r\n";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // labelTaskDescription
+            // 
+            this.labelTaskDescription.AutoSize = true;
+            this.labelTaskDescription.Location = new System.Drawing.Point(12, 9);
+            this.labelTaskDescription.Name = "labelTaskDescription";
+            this.labelTaskDescription.Size = new System.Drawing.Size(532, 80);
+            this.labelTaskDescription.TabIndex = 8;
+            this.labelTaskDescription.Text = resources.GetString("labelTaskDescription.Text");
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTaskDescription);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonMonthIncrease);
             this.Controls.Add(this.textBoxLimitDeposit);
             this.Controls.Add(this.TextLableLimitDeposit);
             this.Controls.Add(this.textBoxLimitIncomeDeposit);
@@ -132,8 +144,9 @@
         private System.Windows.Forms.TextBox textBoxLimitIncomeDeposit;
         private System.Windows.Forms.Label TextLableLimitDeposit;
         private System.Windows.Forms.TextBox textBoxLimitDeposit;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonMonthIncrease;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelTaskDescription;
     }
 }
 
