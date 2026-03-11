e2ba486c4c36bb8008528d4edf85ea2d73284cbd
Sargorus <Dima35206@yandex.ru>
2026-03-11 09:47:15 +0800

    Добавлены макеты кнопок и текстовых полей и полей ввода.

--- CcharpWinFormsTask2/Form1.Designer.cs
+++ CcharpWinFormsTask2/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.textBoxLimitIncomeDeposit = new System.Windows.Forms.TextBox();
             this.TextLableLimitDeposit = new System.Windows.Forms.Label();
             this.textBoxLimitDeposit = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxInitialDeposit
@@ -84,11 +86,31 @@
             this.textBoxLimitDeposit.Size = new System.Drawing.Size(97, 22);
             this.textBoxLimitDeposit.TabIndex = 5;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBoxLimitDeposit);
             this.Controls.Add(this.TextLableLimitDeposit);
             this.Controls.Add(this.textBoxLimitIncomeDeposit);
@@ -110,6 +132,8 @@
         private System.Windows.Forms.TextBox textBoxLimitIncomeDeposit;
         private System.Windows.Forms.Label TextLableLimitDeposit;
         private System.Windows.Forms.TextBox textBoxLimitDeposit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
